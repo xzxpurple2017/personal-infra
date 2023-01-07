c8418b79babe512a595586fd3dcccb99cace54c2
xzxpurple2017 <philipdam@hotmail.com>
2023-01-06 17:42:42 -0800

    Verify that commits are signed Install Ansible Galaxy modules Clean up remnants after running and install Improved informational messages

--- ansible/ansible-pull-bootstrap.sh
+++ ansible/ansible-pull-bootstrap.sh
@@ -42,7 +42,7 @@ elif [[ "$os" = "centos-stream-9" ]] ; then
     dnf update -y
     dnf install -y epel-release
     sleep 10
-    dnf install -y ansible git #python3-boto3
+    dnf install -y ansible-core git #python3-boto3
 elif [[ "$os" = "ubuntu1804" ]] ; then
     apt-get update
     sleep 10
@@ -60,6 +60,16 @@ else
 fi
 echo "# OS is $os"
 
+# Import GPG public keys that are allowed to sign commits on this repo
+declare -a gpg_pub_key_list=(
+    9BA47E6D4ABD047DA5FEA59CAF802A2CCABEEDE1
+)
+
+for key in ${gpg_pub_key_list[@]} ; do
+    gpg --keyserver keyserver.ubuntu.com --recv-keys ${key}
+done
+
+# Create env file
 echo "# Creating Ansible environment variable file"
 cat > /etc/ansible/ansible-pull.env <<-EOF
 GIT_REPO=git@github.com:xzxpurple2017/personal-infra.git
@@ -76,11 +86,25 @@ EOF
 echo "# Writing Github deploy key"
 cat > /etc/ansible/ansible-pull.key <<-EOF
 -----BEGIN OPENSSH PRIVATE KEY-----
-REPLACE ME
+REPLACE_ME
 -----END OPENSSH PRIVATE KEY-----
 EOF
 
 chmod 400 /etc/ansible/ansible-pull.key
+
+# Install Ansible Galaxy modules
+echo "# Installing Ansible Galaxy modules"
+tmp_git_dir=$( mktemp -d -t XXXXXXXX )
+git clone \
+    -c core.sshCommand="/usr/bin/ssh \
+      -o IdentitiesOnly=yes \
+      -i /etc/ansible/ansible-pull.key" \
+    git@github.com:xzxpurple2017/personal-infra.git \
+    ${tmp_git_dir}
+
+ansible-galaxy install -r ${tmp_git_dir}/ansible/${os}/requirements.yml
+rm -rf ${tmp_git_dir}
+echo
 
 echo "# Creating Ansible pull systemd unit file"
 cat > /etc/systemd/system/ansible-pull.service <<-EOF
@@ -95,8 +119,10 @@ ExecStart=/bin/bash -c "(/bin/ps aux | /bin/grep '/usr/bin/ansible-pull' | /bin/
   -C \$GIT_BRANCH \\
   -d \$GIT_PATH \\
   -c local \\
+  --verify-commit \\
   --key-file \$GIT_PRIVATE_KEY_PATH \\
-  \$PLAYBOOK_FILE"
+  \$PLAYBOOK_FILE" \\
+  && rm -rf \$GIT_PATH
 Type=oneshot
 User=root
 Group=root
@@ -119,12 +145,17 @@ EOF
 echo "# Adding github.com RSA fingerprint to known_hosts"
 ssh-keyscan -t rsa github.com > /root/.ssh/known_hosts
 chmod 644 /root/.ssh/known_hosts
+echo
 
 # Start up the Ansible services
-echo "# Initializing Ansible pull in systemd - this could take a while"
+echo "# Initializing Ansible pull in systemd"
 systemctl daemon-reload
 systemctl enable ansible-pull.service
 systemctl enable ansible-pull.timer
+echo
+
+echo "# Running Ansible - this could take a while"
+echo "# NOTE: Commits must be signed with a valid PGP key"
 systemctl start ansible-pull.service
 systemctl start ansible-pull.timer
 
